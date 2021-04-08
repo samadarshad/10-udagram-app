f4a8c22db5c723a6aadae00e639cd242da57a9ec
samad <samadarshad@hotmail.co.uk>
2021-04-08 14:55:51 +0100

    notifications

--- src/functions/s3/sendNotifications/handler.ts
+++ src/functions/s3/sendNotifications/handler.ts
@@ -1,11 +1,66 @@
 import { S3Handler, S3Event } from 'aws-lambda'
 import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const connectionsTable = process.env.CONNECTIONS_TABLE
+const stage = process.env.STAGE
+const apiId = process.env.API_ID
+
+const connectionsParams = {
+    apiVersion: "2021-04-08",
+    endpoint: `${apiId}.execute-api.eu-west-2.amazonaws.com/${stage}`
+}
+
+const apiGateway = new AWS.ApiGatewayManagementApi(connectionsParams)
 
 export const handler: S3Handler = async (event: S3Event) => {
     for (const record of event.Records) {
         const key = record.s3.object.key
-        console.log('Processing S3 item with key: ', key);
-        
+        console.log('Processing S3 item with key: ', key); 
+
+        const connections = await docClient.scan({
+            TableName: connectionsTable
+        }).promise()
+
+        const payload = {
+            imageId: key
+        }
+
+        for (const connection of connections.Items) {
+            const connectionId = connection.id
+            await sendMessageToClient(connectionId, payload)
+        }
+
     }
+
+
+}
+
+async function sendMessageToClient(connectionId, payload) {
+    try {
+        console.log('Sending message to a connection', connectionId);
+
+        await apiGateway.postToConnection({
+            ConnectionId: connectionId,
+            Data: JSON.stringify(payload)
+        }).promise()
+
+    } catch (e) {
+        console.log('Failed to send message', JSON.stringify(e));
+        if (e.statusCode === 410) {
+            console.log('Stale connection');
+
+            await docClient.delete({
+                TableName: connectionsTable,
+                Key: {
+                    id: connectionId
+                }
+            }).promise()
+            
+        }
+    }      
+    
 }
 
--- src/functions/s3/sendNotifications/index.ts
+++ src/functions/s3/sendNotifications/index.ts
@@ -10,5 +10,11 @@ export default {
         existing: true,
       }
     }
-  ]
+  ],
+  environment: {
+    STAGE: "${self:provider.stage}",
+    API_ID: {
+      Ref: "WebsocketsApi"
+    }
+  }
 }
