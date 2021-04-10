6b2a9898c7d417062ea68cd2c4eddecee1b6d7c3
samad <samadarshad@hotmail.co.uk>
2021-04-10 19:44:39 +0100

    Auth0 jwt

--- environment.ts
+++ environment.ts
@@ -7,5 +7,6 @@ export default {
     IMAGE_ID_INDEX: "ImageIdIndex",
     IMAGES_S3_BUCKET: "serverless-udagram-${self:provider.environment.accountId}-images-${self:provider.stage}",
     SIGNED_URL_EXPIRATION: '300',
-    THUMBNAILS_S3_BUCKET: 'serverless-udagram-thumbnail-${self:provider.environment.accountId}-${self:provider.stage}'
+    THUMBNAILS_S3_BUCKET: 'serverless-udagram-thumbnail-${self:provider.environment.accountId}-${self:provider.stage}',
+    AUTH_0_SECRET: '_8DzGGdiF7kwbkazhw5W7AjzPboJ0I_b3mlvt69-V0ds6QQ96jVHylBxuiYtoMwP'
   }
--- src/functions/auth/auth0Authorizer/handler.ts
+++ src/functions/auth/auth0Authorizer/handler.ts
@@ -1,18 +1,23 @@
 import { APIGatewayAuthorizerEvent, APIGatewayAuthorizerResult, APIGatewayAuthorizerHandler } from 'aws-lambda'
 import 'source-map-support/register'
 
+import { verify } from 'jsonwebtoken'
+import { JwtToken } from '../../../auth/JwtToken'
+
+const auth0Secret = process.env.AUTH_0_SECRET
+
 export const handler: APIGatewayAuthorizerHandler = async (event: APIGatewayAuthorizerEvent): Promise<APIGatewayAuthorizerResult> => {
     try {
-        verifyToken(event.authorizationToken)
+        const decodedToken = verifyToken(event.authorizationToken)
         console.log('User was authorized');
-        return allowAllIamPolicyStatement        
+        return allowAllIamPolicyStatement(decodedToken.sub)        
     } catch (error) {
         console.log('User was not authorized', error.message);
         return denyAllIamPolicyStatement    
     }    
 }
 
-function verifyToken(authHeader: string) {
+function verifyToken(authHeader: string): JwtToken {
     if (!authHeader)
         throw new Error('No authorization header')
 
@@ -22,24 +27,24 @@ function verifyToken(authHeader: string) {
     const split = authHeader.split(' ')
     const token = split[1]
 
-    if (token !== '123')
-        throw new Error('Invalid token')
 
-    // A request has been authorized
+    return verify(token, auth0Secret) as JwtToken
 }
 
 
-const allowAllIamPolicyStatement = {
-    principalId: 'user',
-    policyDocument: {
-        Version: '2012-10-17',
-        Statement: [
-            {
-                Action: 'execute-api:Invoke',
-                Effect: 'Allow',
-                Resource: '*'
-            }
-        ]
+const allowAllIamPolicyStatement = (userId) => {
+    return {
+        principalId: userId,
+        policyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Action: 'execute-api:Invoke',
+                    Effect: 'Allow',
+                    Resource: '*'
+                }
+            ]
+        }
     }
 }
 
